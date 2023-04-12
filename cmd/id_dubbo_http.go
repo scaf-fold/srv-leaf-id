4c35a79287ce7fe32bd5f98919e4fd7ddc6cb571
dunbar <wxfsirius@163.com>
2023-04-12 16:26:08 +0800

    feat: modify
    Signed-off-by: dunbar <wxfsirius@163.com>

--- cmd/id_dubbo_http.go
+++ cmd/id_dubbo_http.go
@@ -3,23 +3,30 @@ package main
 import (
 	"dubbo.apache.org/dubbo-go/v3/config"
 	_ "dubbo.apache.org/dubbo-go/v3/imports"
+	"github.com/gin-gonic/gin"
+	"github.com/scaf-fold/srv-leaf-id/controller"
 	"github.com/scaf-fold/srv-leaf-id/global"
 	"github.com/scaf-fold/srv-leaf-id/pkg/id_rpc"
+	"github.com/scaf-fold/tools/pkg/courier"
 	"log"
 )
 
 func init() {
 	global.Config(nil)
+	log.Println("id service initial started")
 }
 
 func init() {
 	config.SetProviderService(&id_rpc.IdGenerator{})
-}
-
-func main() {
 	if err := config.Load(); err != nil {
 		panic(err)
 	}
 	log.Println("id rpc provider started")
-	select {}
+}
+
+func main() {
+	g := gin.Default()
+	courier.App(g, controller.Root, func(e *gin.Engine) {
+		e.Run(":8081")
+	})
 }
