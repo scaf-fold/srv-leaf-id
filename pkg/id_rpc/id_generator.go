07ea0d30245484730b162e1ffa65d5275f3a05fe
dunbar <wxfsirius@163.com>
2023-04-14 09:20:53 +0800

    feat: impl grpc
    Signed-off-by: dunbar <wxfsirius@163.com>

--- pkg/id_rpc/id_generator.go
+++ pkg/id_rpc/id_generator.go
@@ -2,7 +2,10 @@ package id_rpc
 
 import (
 	"context"
+	"errors"
 	"github.com/scaf-fold/srv-leaf-id/api"
+	"github.com/scaf-fold/srv-leaf-id/global"
+	"strconv"
 )
 
 type IdGenerator struct {
@@ -10,11 +13,35 @@ type IdGenerator struct {
 }
 
 func (id *IdGenerator) Id(context.Context, *api.IdRequest) (*api.IdResponse, error) {
-	return nil, nil
+	ids, err := global.ID.Ids(1)
+	if err != nil {
+		return nil, err
+	}
+	if ids != nil && len(ids) > 0 {
+		return &api.IdResponse{
+			Id: ids[0],
+		}, nil
+	}
+	return nil, errors.New("StatusInternalServerError")
 }
 func (id *IdGenerator) Ids(context.Context, *api.IdBatchRequest) (*api.IdBatchResponse, error) {
-	return nil, nil
+	ids, err := global.ID.Ids(1)
+	if err != nil {
+		return nil, err
+	}
+	return &api.IdBatchResponse{
+		Ids: ids,
+	}, nil
 }
-func (id *IdGenerator) Inverse(context.Context, *api.IdInverseRequest) (*api.IdInverseResponse, error) {
-	return nil, nil
+func (id *IdGenerator) Inverse(_ context.Context, r *api.IdInverseRequest) (*api.IdInverseResponse, error) {
+	snow, err := global.ID.IdInverse(r.Id, global.StartTime)
+	if err != nil {
+		return nil, err
+	}
+	return &api.IdInverseResponse{
+		Id:       snow.Id,
+		Time:     snow.GenerateTime.String(),
+		Sequence: strconv.FormatInt(snow.Sequence, 10),
+		WorkId:   strconv.FormatInt(snow.WorkId, 10),
+	}, nil
 }
